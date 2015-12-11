756c7ed75856ab963ce85fdfd424f1ef1996c2cc
Cameron Paul <cpaul37@gmail.com>
2015-12-11 00:22:00 -0600

    Don't parse args in the request handler

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use docopt::Docopt;
 use hyper::Client;
 use hyper::Server;
 use hyper::header::Connection;
+use hyper::server::Handler;
 use hyper::server::Request;
 use hyper::server::Response;
 use hyper::uri::RequestUri;
@@ -24,32 +25,40 @@ struct Args {
     arg_proxy: String,
 }
 
-fn proxy(proxy_request: Request, proxy_response: Response) {
+struct ProxyHandler {
+    uri_base: String,
+}
+
+impl Handler for ProxyHandler {
+    fn handle(&self, proxy_request: Request, proxy_response: Response) {
+        let client = Client::new();
+
+        match (proxy_request.method, proxy_request.uri) {
+            (hyper::Get, RequestUri::AbsolutePath(ref path)) => {
+                let uri = self.uri_base.clone() + &path;
+                let mut response = client.get(&uri)
+                                         .header(Connection::close())
+                                         .send()
+                                         .unwrap();
+
+                let mut body = String::new();
+                response.read_to_string(&mut body).unwrap();
+
+                proxy_response.send(&body.into_bytes()).unwrap();
+            }
+            _ => {}
+        }
+    }
+}
+
+fn main() {
     let args: Args = Docopt::new(USAGE)
                          .and_then(|d| d.decode())
                          .unwrap_or_else(|e| e.exit());
 
     let uri_base = "http://".to_string() + &args.arg_proxy;
 
-    let client = Client::new();
-
-    match (proxy_request.method, proxy_request.uri) {
-        (hyper::Get, RequestUri::AbsolutePath(ref path)) => {
-            let uri = uri_base + &path;
-            let mut response = client.get(&uri)
-                                     .header(Connection::close())
-                                     .send()
-                                     .unwrap();
-
-            let mut body = String::new();
-            response.read_to_string(&mut body).unwrap();
-
-            proxy_response.send(&body.into_bytes()).unwrap();
-        }
-        _ => {}
-    }
-}
-
-fn main() {
-    Server::http("127.0.0.1:3000").unwrap().handle(proxy);
+    Server::http("127.0.0.1:3000").unwrap().handle(ProxyHandler {
+        uri_base: uri_base,
+    });
 }
